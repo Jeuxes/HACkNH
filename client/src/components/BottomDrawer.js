e24cd986da6694b32af54592b9cbb98ce51edfd1
iamowillis <iamowillis@gmail.com>
2024-11-09 17:17:50 -0500

    Fix for bottomDrawer

--- client/src/components/BottomDrawer.js
+++ client/src/components/BottomDrawer.js
@@ -6,57 +6,61 @@ import CloseIcon from '@mui/icons-material/Close';
 
 const screenHeight = Dimensions.get('window').height;
 
-const BottomDrawer = forwardRef(({ isVisible, children, onClose, }, ref) => {
-    const [currentSnap, setCurrentSnap] = useState(0);
+const BottomDrawer = forwardRef(({ isVisible, onClose, children, }, ref) => {
+    const [isFullyOpen, setFullyOpen] = React.useState(false);
     const panY = useRef(new Animated.Value(screenHeight)).current;
-
+    
     const resetPositionAnim = Animated.timing(panY, {
         toValue: 0, duration: 200, useNativeDriver: true,
     });
-
+    
     const closeAnim = Animated.timing(panY, {
-        toValue: screenHeight,
+        toValue: screenHeight/2,
         duration: 400,
         useNativeDriver: true,
     });
-
+    
     const fullScreenAnim = Animated.timing(panY, {
-        toValue: -300, // Would move modal to full screen
+        toValue: -600, // Would move modal to full screen
         duration: 500,
         useNativeDriver: true,
     });
-
+    
     const panResponders = useRef(
-        PanResponder.create({
-            onStartShouldSetPanResponder: () => true,
-            onPanResponderMove: Animated.event([null, { dy: panY }], { useNativeDriver: false }),
-            onPanResponderRelease: (e, gs) => {
-                if (gs.dy > 0 && gs.vy > 2) {
-                    return closeAnim.start(() => onClose());
-                }
-                // else if (gs.dy < -200) { // Assuming -100 is your threshold for swiping up to go full screen
-                //     console.log("Starting full screen")
-                //     resetPositionAnim.stop();
-                //     return fullScreenAnim.start(); // Transition to full screen
-                // }
-                return resetPositionAnim.start();
-            },
-        })
+      PanResponder.create({
+          // Allow pan responder to activate
+          onStartShouldSetPanResponder: () => true,
+          // Handle card movement while dragging
+          onPanResponderMove: Animated.event([null, { dy: panY }], { useNativeDriver: false }),
+          // Handle card release after dragging
+          onPanResponderRelease: (e, gs) => {
+              if (gs.dy > 0 && gs.vy > 2) {
+                  console.log("Swipe down --- Closing drawer")
+                  return closeAnim.start(() => onClose());
+              } else if (gs.dy < -100) { // Assuming -100 is your threshold for swiping up to go full screen
+                  console.log("Starting full screen")
+                  return fullScreenAnim.start(() => setFullyOpen(true)); // Transition to full screen
+              }
+              return resetPositionAnim.start();
+          },
+      })
     ).current;
-
+    
     useEffect(() => {
         if (isVisible) {resetPositionAnim.start();}
-        else {closeAnim.start();}
+        else {
+            closeAnim.start(() => onClose());
+        }
     }, [isVisible, closeAnim, resetPositionAnim]);
 
     return (
-        <Modal visible={isVisible} backdropColor={"transparent"} style={styles.modal} presentationStyle={'overFullScreen'}>
+        <Modal backdropColor={false} transparent={true} visible={isVisible} style={styles.modal} presentationStyle={'overFullScreen'}>
             <Animated.View style={[styles.drawerContainer, { transform: [{ translateY: panY }] }]}>
                 <View style={styles.container}>
                     <TextIconButton
                         style={[styles.button, styles.close_button]}
                         onPress={() => {
-                            closeAnim.start()
+                            closeAnim.start(() => onClose())
                         }}
                         icon={<CloseIcon color={styles_.DARK_GRAY} size={28}/>}
                     />
@@ -92,7 +96,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         borderTopRightRadius: 20,
         borderTopLeftRadius: 20,
-        maxHeight: '100%',
+        maxHeight: '60%',
     },
     horizontalBar: {
         width: 100,
