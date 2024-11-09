33bd5f74410487cdeb02490bfe773e306a38fe38
Will Leverone <barrelwolf38087@gmail.com>
2024-11-09 16:01:28 -0500

    backend user management

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,9 +4,43 @@ import {packInterests} from '../util/interests.js'
 
 
 // To store location/match/socket info
+/**
+ * {
+ *   "user ID": {
+ *     name: "Name",
+ *     location: [lat, long],
+ *     currentVenue: "Venue Name (must match key in venues)",
+ *     socketId: socketId
+ *   }
+ * }
+ */
 export let activeUsers = {}
 
-export const login = async (req, res) => {
+/**
+ * {
+ *   "Venue Name": {
+ *     location: "(lat, long)",
+ *     type: "type"
+ *   }
+ * }
+ */
+export let venues = {}
+
+
+const getUser = async (uid) => {
+    const query = `SELECT * FROM users WHERE uid=${uid};`
+    
+    const result = await pool.query(query)
+
+    if (result.rowCount != 1) {
+        console.error(`Query for uid ${uid} returned ${result.rowCount} rows!`)
+        return null;
+    }
+
+    return result.rows[0]
+}
+
+export const register = async (req, res) => {
     const post = req.body
 
 
@@ -35,19 +69,24 @@ export const login = async (req, res) => {
 }
 
 export const updateLoc = (req, res) => {
-    const post = req.body
+    res.status(200).json(null) // temp so client doesn't wait
+    // const post = req.body
 
-    try {
+    // try {
 
-        const uid = post['uid']
-        const lat = post['lat']
-        const long = post['long']
+    //     const uid = post['uid']
+    //     const lat = post['lat']
+    //     const long = post['long']
 
 
-    } catch (err) {
-        res.status(422).json({message: err.message})
-    }
+    // } catch (err) {
+    //     res.status(422).json({message: err.message})
+    // }
     
+}
+
+export const getVenues = (req, res) => {
+
 }
 
 export const startListener = (io) => {
@@ -55,30 +94,41 @@ export const startListener = (io) => {
         console.log('A user connected:', socket.id);
   
         // Register a listener to handle user identification after connection
-        socket.on('register', (uid) => {
-            activeUsers[uid] = socket.id; // Map userId to their socket ID
+        socket.on('register', async (uid) => {
+            if (!(uid in activeUsers)) {
+                const userData = await getUser(uid)
+                activeUsers[uid] = {
+                    name: userData.name,
+                    location: null,
+                    currentVenue: null,
+                    socketId: socket.id
+                }
+            } else {
+                activeUsers[uid].socketId = socket.id
+            }
+
             console.log(`User ${uid} is registered with socket ID: ${socket.id}`);
         });
   
         // Listen for direct messages
         socket.on('sendMessage', (messageData) => {
-            const { senderId, recipientId, content, timestamp } = messageData;
+            const { senderId, content, timestamp } = messageData;
             console.log(`Message from User ${senderId} to User ${recipientId}: ${content} (Timestamp: ${timestamp})`);
   
             // Find the recipient's socket ID
-            const recipientSocketId = activeUsers[recipientId];
-            if (recipientSocketId) {
-                // Send the message to the recipient
-                io.to(recipientSocketId).emit('newMessage', { senderId, content, timestamp });
-            } else {
-                console.log(`User ${recipientId} is not connected.`);
-            }
+            // const recipientSocketId = activeUsers[recipientId];
+            // if (recipientSocketId) {
+            //     // Send the message to the recipient
+            //     io.to(recipientSocketId).emit('newMessage', { senderId, content, timestamp });
+            // } else {
+            //     console.log(`User ${recipientId} is not connected.`);
+            // }
         });
   
         // Handle user disconnect
         socket.on('disconnect', () => {
             const disconnectedUserId = Object.keys(activeUsers).find(
-                (uid) => activeUsers[uid] === socket.id
+                (uid) => activeUsers[uid].socketId === socket.id
             );
             if (disconnectedUserId) {
                 delete activeUsers[disconnectedUserId];
