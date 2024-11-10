0e4033b73d3cee83ee5db60e5a465dc28bcfa7a8
Jahn <jonathandoherty23@gmail.com>
2024-11-10 06:01:08 -0500

    finally progress

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,11 @@ const App = () => {
   };
 
   const initializeSocket = (id) => {
-    if (socket) {
+    if (!socket) {
+      socket = io(SOCKET_URL, {
+        withCredentials: true,
+      });
+
       console.log("Registering user", id);
       socket.emit('register', id);
 
@@ -40,33 +44,21 @@ const App = () => {
     }
   };
 
-  const testConnection = async () => {
-    try {
-      const response = await fetch(`${API_BASE_URL}/`, { method: 'GET' });
-      const data = await response.text();
-      console.log('Server response:', data);
-    } catch (error) {
-      console.error('Error connecting to server:', error);
-    }
-  };
-
   useEffect(() => {
     const navBar = document.querySelector('#navbar');
     if (navBar) {
       setNavBarHeight(navBar.offsetHeight);
     }
 
-    testConnection();
-
     // Clean up WebSocket on unmount
     return () => {
       if (socket) {
-        console.log(`User ${socket.id}: disconnecting`);
         socket.disconnect();
         socket = null;
       }
     };
   }, []);
+
 
 
   return (
