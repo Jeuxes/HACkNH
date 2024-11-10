88865dad51540733b6f96d27b4085bb918e8486d
Jahn <jonathandoherty23@gmail.com>
2024-11-10 02:43:23 -0500

    index changes

--- server/index.js
+++ server/index.js
@@ -43,6 +43,6 @@ const io = new Server(server, {
 startListener(io);
 
 // Start the server
-server.listen(port, () => {
+server.listen(port, '0.0.0.0', () => {
     console.log(`Server listening on port ${port}`);
 });
